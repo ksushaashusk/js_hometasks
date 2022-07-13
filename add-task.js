e2d320993fdc45f2b3511f864054abb4586a4a26
ksushaashusk <100611738+ksushaashusk@users.noreply.github.com>
2022-07-13 16:12:58 +0300

    kursovaya

---
+++ add-task.js
@@ -0,0 +1,76 @@
+﻿"use strict";
+
+// add task
+/* После успешного заполнения формы, данные о задаче необходимо собрать в объект
+
+{
+    title: название задачи,
+        description: описание задачи,
+            date: срок выполнения задачи // дату выставлять через date. ПРОВЕРИТЬ ВРЕМЯ Date()настоящее время.
+}
+Объект может содержать дополнительные свойства
+Собранный объект добавляется в локальное хранилище. // Объект помещается в локальное хранилище в формате json 
+Задачи хранятся в сериализованном массиве (JSON строка) по ключу "tasks".*/
+
+
+document.forms["add-task"].elements.title.addEventListener("input", function () {
+    if (this.validity.tooShort) {
+        this.nextElementSibling.innerText = "Значение должно быть от 1 до 20 символов";
+        return;
+    }
+    if (this.validity.tooLong) {
+        this.nextElementSibling.innerText = "Значение должно быть от 1 до 20 символов";
+        return;
+    }
+    this.nextElementSibling.innerText = "Поле заполнено верно";
+})
+
+
+document.forms["add-task"].elements.date.addEventListener("input", function () {
+    let date = new Date(document.getElementById("date").value);
+    let today = new Date();
+    if (date.getTime() <= today.getTime()) {
+        this.nextElementSibling.innerText = "Дата не может быть в прошлом";
+    }
+    else {
+        this.nextElementSibling.innerText = "Дата назначена";
+    }
+        
+    
+})
+
+
+
+   
+
+
+function addTask(event) {
+    event.preventDefault();
+    let title = this.elements.title;
+    title = title.value;
+
+    let description = this.elements.description.value;
+
+    let date = this.elements.date;
+    date = date.value;
+
+    let taskInfo = { title, description, date };
+   // taskInfo = JSON.stringify(taskInfo); образовать элемент в строку
+    let tasks = localStorage.getItem("tasks");
+    if (tasks) tasks = JSON.parse(tasks); 
+    else tasks = [];
+
+    tasks.push(taskInfo); 
+    console.log(JSON.stringify(tasks));
+    localStorage.setItem("tasks", JSON.stringify(tasks));
+
+    this.elements.submit.nextElementSibling.innerText = "Задача успешно добавлена";
+}
+
+document.forms["add-task"].addEventListener("submit", addTask);
+
+
+
+
+
+
---
+++ all-tasks.js
@@ -0,0 +1,81 @@
+﻿/* Для вывода необходимо получить все задачи из локального хранилища. 
+Если в хранилище нет задач вывести информацию об этом в html.
+
+Реализовать возможность выделения цветом задач, которые необходимо удалить и их удаление при нажатии на кнопку 'Удалить выбранные'.
+
+Задачи должны удаляться и из локального хранилища и из html.
+
+Перечень полей формы должен быть как на изображении(add - task.png).
+
+Стилевое оформление страниц может быть любым.*/ 
+
+
+
+"use strict";
+
+let taskSection = document.getElementById("tasks");
+
+let tasks = localStorage.getItem("tasks");
+if (!tasks) {
+    taskSection.innerText = "В списке нет задач";
+}
+else {
+    tasks = JSON.parse(tasks);
+
+        tasks.map(task => {
+            let wrapper = document.createElement("div");
+            wrapper.className = "task";
+              
+
+        let title = document.createElement("h2");
+        let description = document.createElement("p");
+        let date = document.createElement("p2");
+
+        title.innerText = task.title;
+        wrapper.appendChild(title);
+        description.innerText = task.description;
+        wrapper.appendChild(description);
+        date.innerText = "Выполнить к: " + task.date;
+        wrapper.appendChild(date);
+
+        document.body.appendChild(wrapper);
+        });
+}
+
+
+// при нажатии на div можно его удалить, но при обновлении страницы пункт возвращается, удаляется только из html 
+let container = document.querySelectorAll(".task");
+
+for (let c of container) {
+    c.addEventListener("click", container_click);
+}
+
+function container_click() {
+    this.classList.toggle("active");  
+ }
+
+ // удаление из html
+let del = document.getElementById("delete");
+del.addEventListener("click", () => {
+    let elems = document.querySelectorAll(".active");
+    let tasks = localStorage.getItem("tasks");
+    tasks = JSON.parse(tasks);
+
+    for (let elem of elems) {
+        tasks.splice(tasks.findIndex(i => i.id === elem.id), 1);
+        elem.remove();
+    }
+    localStorage.setItem("tasks", JSON.stringify(tasks));
+})
+
+
+
+// console.log(container); nodeList [div.task, div.task, div.task, div.task, div.task, div.task]
+
+
+        
+        
+
+
+
+   
